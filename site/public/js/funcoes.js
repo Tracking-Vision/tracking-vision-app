085482d785a76ebf560be6ef514bf13e7370b723
Samuel-Oli-P <samuel.pereira@sptech.school>
2023-04-19 17:40:58 -0300

    Feat: Dashboard design atualizado

--- site/public/js/funcoes.js
+++ site/public/js/funcoes.js
@@ -24,32 +24,20 @@ function limparSessao() {
     window.location = "../login.html";
 }
 
-// carregamento (loading)
-// function aguardar() {
-//     var divAguardar = document.getElementById("div_aguardar");
-//     divAguardar.style.display = "flex";
-// }
+function MenuShow() {
+    menu.classList.remove("menu_move_close");
+    menu.classList.toggle("menu_move_open");
+    menuOpen.classList.toggle("menuOpen");
+    setTimeout(() => {
+        menuClose.classList.toggle("menuClose");
+        containerClose.classList.toggle("closeHidden");
+    }, 200)
+}
 
-// function finalizarAguardar(texto) {
-//     var divAguardar = document.getElementById("div_aguardar");
-//     divAguardar.style.display = "none";
-
-//     var divErrosLogin = document.getElementById("div_erros_login");
-//     if (texto) {
-//         divErrosLogin.style.display = "flex";
-//         divErrosLogin.innerHTML = texto;
-//     }
-// }
-
-
-// modal
-// function mostrarModal() {
-//     var divModal = document.getElementById("div_modal");
-//     divModal.style.display = "flex";
-// }
-
-// function fecharModal() {
-//     var divModal = document.getElementById("div_modal");
-//     divModal.style.display = "none";
-// }
-
+function MenuHidden() {
+    menu.classList.remove("menu_move_open");
+    menu.classList.toggle("menu_move_close");
+    menuClose.classList.toggle("menuClose");
+    menuOpen.classList.toggle("menuOpen");
+    containerClose.classList.remove("closeHidden");
+}
